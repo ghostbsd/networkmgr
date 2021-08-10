1f65b136b1ba6143ed4ca658475647823735a6f8
ericbsd <ericturgeon.bsd@gmail.com>
2021-08-09 21:57:54 -0300

    update version in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from platform import system
 from setuptools import setup
 from subprocess import run
 
-__VERSION__ = '5.8'
+__VERSION__ = '5.9'
 PROGRAM_VERSION = __VERSION__
 
 prefix = '/usr/local' if system() == 'FreeBSD' else sys.prefix
