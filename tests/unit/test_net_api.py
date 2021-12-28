f5c9443988b7006404fd034e7fbeb61eae71e6ce
Ron Georgia <rong1611@gmail.com>
2021-12-27 19:38:35 -0500

    sorted imports for test_net_

--- tests/unit/test_net_api.py
+++ tests/unit/test_net_api.py
@@ -5,40 +5,40 @@ top_dir = str(Path(__file__).absolute().parent.parent.parent)
 
 try:
     from src.net_api import (
-        stopnetworkcard,
-        startnetworkcard,
-        wifiDisconnection,
-        stopallnetwork,
-        startallnetwork,
+        card_online,
+        connectionStatus,
         connectToSsid,
+        defaultcard,
+        delete_ssid_wpa_supplicant_config,
         disableWifi,
         enableWifi,
-        connectionStatus,
         networkdictionary,
         openrc,
-        delete_ssid_wpa_supplicant_config,
+        startallnetwork,
+        startnetworkcard,
+        stopallnetwork,
+        stopnetworkcard,
+        wifiDisconnection,
         wlan_status,
-        card_online,
-        defaultcard
     )
 except ImportError:
     sys.path.append(top_dir)
     from src.net_api import (
-        stopnetworkcard,
-        startnetworkcard,
-        wifiDisconnection,
-        stopallnetwork,
-        startallnetwork,
+        card_online,
+        connectionStatus,
         connectToSsid,
+        defaultcard,
+        delete_ssid_wpa_supplicant_config,
         disableWifi,
         enableWifi,
-        connectionStatus,
         networkdictionary,
         openrc,
-        delete_ssid_wpa_supplicant_config,
+        startallnetwork,
+        startnetworkcard,
+        stopallnetwork,
+        stopnetworkcard,
+        wifiDisconnection,
         wlan_status,
-        card_online,
-        defaultcard
     )
 
 
@@ -46,10 +46,12 @@ def test_defaultcard_returns_str():
     result = defaultcard()
     assert isinstance(result, str)
 
+
 def test_card_online():
     netcard = defaultcard()
     result = card_online(netcard)
     assert result
+
 
 def test_card_not_online():
     netcard = "em99"
