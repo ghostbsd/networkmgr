6811744b7bc6f3a6a36f4fe11a55def2fa3aa0f5
Alexander Alexeev <alexax66@gmail.com>
2022-08-29 20:39:52 +0300

    setup.py: fix typos

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ PROGRAM_VERSION = __VERSION__
 
 prefix = '/usr/local' if system() == 'FreeBSD' else sys.prefix
 
-# compiling trainslations
+# compiling translations
 os.system("sh compile_translations.sh")
 
 def datafilelist(installbase, sourcebase):
